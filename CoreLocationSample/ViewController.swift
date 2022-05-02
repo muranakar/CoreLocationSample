1a204449cbafbfb104e584b70affaa643fbad343
muranakar <p1211065@gmail.com>
2022-05-03 05:19:17 +0900

    Initial Commit

---
+++ CoreLocationSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreLocationSample
+//
+//  Created by 村中令 on 2022/05/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
