8514a822b4d2684f1c212dda458eacb151ef61fd
muranakar <p1211065@gmail.com>
2022-05-04 09:53:00 +0900

    map

---
+++ CoreLocationSample/PediatricWelfareService.swift
@@ -0,0 +1,21 @@
+//
+//  PediatricWelfareService.swift
+//  CoreLocationSample
+//
+//  Created by 村中令 on 2022/05/03.
+//
+
+import Foundation
+
+struct PediatricWelfareService {
+    let officeNumber: String
+    let serviceType: String
+    let corporateName: String
+    let corporateKana: String
+    let officeName: String
+    let officeNameKana: String
+    let postalCode: String
+    let address: String
+    let telephoneNumber: String
+    let fax: String
+}
